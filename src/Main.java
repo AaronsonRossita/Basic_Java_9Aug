869967b079e3729d12004fc6850d0bfe4b5f8c85
Rossita <47862499+AaronsonRossita@users.noreply.github.com>
2023-08-06 21:51:16 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String str1 = "HellO";
+        String str2 = "heLLo";
+
+
+        String str1LowerCase = str1.toLowerCase();
+        String str2LowerCase = str2.toLowerCase();
+
+//        System.out.println("str1 = " + str1);
+//        System.out.println("str2 = " + str2);
+//        System.out.println("--------");
+//        System.out.println("str1LowerCase = " + str1LowerCase);
+//        System.out.println("str2LowerCase = " + str2LowerCase);
+//        System.out.println(str1LowerCase.equals(str2LowerCase));
+//
+//        System.out.println(str1.length());
+//
+//        System.out.println(str1.charAt(str1.length()-1));
+        System.out.println(str1.indexOf("all"));
+
+
+
+
+    }
+}
